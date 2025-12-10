7f7c08d2f9acbbcfa2a40bc33b1d592f56a9af21
LiuKun <2233549444@qq.com>
2025-12-10 15:51:09 +0800

    update Button3D

--- Runtime/Components/Button3D.cs
+++ Runtime/Components/Button3D.cs
@@ -4,19 +4,31 @@ using System.Linq;
 using LK.Runtime.Utilities;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 namespace LK.Runtime.Components
 {
     [RequireComponent(typeof(Collider))]
-    public class Button3D : Selectable3D
+    public class Button3D : Selectable3D, IPointerClickHandler
     {
         [field: SerializeField]
         public UnityEvent OnClick { get; set; }
 
-        private void OnMouseUpAsButton()
+        public virtual void OnPointerClick(PointerEventData eventData)
         {
-            if(!isActiveAndEnabled || !Interactable) return;
+            if (eventData.button != PointerEventData.InputButton.Left)
+                return;
+
+            Press();
+        }
+
+        private void Press()
+        {
+            if (!IsActive() || !IsInteractable())
+                return;
             
+            UISystemProfilerApi.AddMarker("Button3D.OnClick", this);
             OnClick?.Invoke();
         }
     }
