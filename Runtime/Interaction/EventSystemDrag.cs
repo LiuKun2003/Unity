f9649511a64bff577798a957b0f419fba06b5fb5
SHUHE-A1\LiuKun <2233549444@qq.com>
2026-04-07 17:21:27 +0800

    修改EventSystemDrag的反转轴向功能

--- Runtime/Interaction/EventSystemDrag.cs
+++ Runtime/Interaction/EventSystemDrag.cs
@@ -1,12 +1,19 @@
+using UnityEngine;
 using UnityEngine.EventSystems;
 
 namespace LK.Runtime.Interaction
 {
     public class EventSystemDrag : InputProviderBase, IDragHandler
     {
+        [SerializeField] private bool reverseX;
+        [SerializeField]  private bool reverseY;
+        
         public void OnDrag(PointerEventData eventData)
         {
-            OutDelta(eventData.delta);
+            var v = new Vector3(eventData.delta.x, eventData.delta.y, 0);
+            if (reverseX) v.x *= -1;
+            if (reverseY) v.y *= -1;
+            OutDelta(v);
         }
     }
 }
