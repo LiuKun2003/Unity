b6b57f513314a3c355886c24ec5eb9324dafb928
SHUHE-A1\LiuKun <2233549444@qq.com>
2026-05-13 15:45:56 +0800

    修改 EventSystemDrag 可以指定按下键

--- Runtime/Interaction/EventSystemDrag.cs
+++ Runtime/Interaction/EventSystemDrag.cs
@@ -5,14 +5,16 @@ namespace LK.Runtime.Interaction
 {
     public class EventSystemDrag : InputProviderBase, IDragHandler
     {
+        [SerializeField] private PointerEventData.InputButton button = PointerEventData.InputButton.Left;
         [SerializeField] private bool reverseX;
-        [SerializeField]  private bool reverseY;
+        [SerializeField] private bool reverseY;
 
         public bool ReverseX { get => reverseX; set => reverseX = value; }
         public bool ReverseY { get => reverseY; set => reverseY = value; }
 
         public void OnDrag(PointerEventData eventData)
         {
+            if(eventData.button != button) return;
             var v = new Vector3(eventData.delta.x, eventData.delta.y, 0);
             if (reverseX) v.x *= -1;
             if (reverseY) v.y *= -1;
